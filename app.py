3e613b952072d116ce9deb3194d201e88475cf13
krishna dasari <krishnadasari610@gmail.com>
2026-02-23 07:31:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello, world!'
-    return 'my python fisrt applicaiton'
+    return 'my python fisrt applicaiton aws code pipeline'
 
 if __name__ == '__main__':
     app.run()
